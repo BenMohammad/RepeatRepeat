ec4596070797f5ba44bb53d5452f40a7118bac17
Ben Mohammad <benmohammad593@gmail.com>
2020-08-04 08:15:04 +0100

    Either_dataenum and xml added...

---
+++ app/src/main/java/com/benmohammad/repeatrepeat/util/Either_dataenum.java
@@ -0,0 +1,11 @@
+package com.benmohammad.repeatrepeat.util;
+
+import com.spotify.dataenum.DataEnum;
+import com.spotify.dataenum.dataenum_case;
+
+@DataEnum
+public interface Either_dataenum<A, B> {
+
+    dataenum_case Left(A value);
+    dataenum_case Right(B value);
+}
