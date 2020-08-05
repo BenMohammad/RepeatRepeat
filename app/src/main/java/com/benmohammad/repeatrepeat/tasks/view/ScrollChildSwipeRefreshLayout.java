eab6309283872440d156be17bf5e37345ce7a031
Ben Mohammad <benmohammad593@gmail.com>
2020-08-05 19:07:22 +0100

    ScrollChildSwipeRefreshLayout.java, TasksListViewDataMapper.java added...

---
+++ app/src/main/java/com/benmohammad/repeatrepeat/tasks/view/ScrollChildSwipeRefreshLayout.java
@@ -0,0 +1,35 @@
+package com.benmohammad.repeatrepeat.tasks.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+public class ScrollChildSwipeRefreshLayout extends SwipeRefreshLayout {
+
+    private View scrollUpChild;
+
+    public ScrollChildSwipeRefreshLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public ScrollChildSwipeRefreshLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean canChildScrollUp() {
+        if(scrollUpChild != null) {
+            return ViewCompat.canScrollVertically(scrollUpChild, -1);
+        }
+        return super.canChildScrollUp();
+    }
+
+    public void setScrollUpChild(View view) {
+        scrollUpChild = view;
+
+    }}
---
+++ app/src/main/java/com/benmohammad/repeatrepeat/tasks/view/TasksListViewDataMapper.java
@@ -0,0 +1,47 @@
+package com.benmohammad.repeatrepeat.tasks.view;
+
+import androidx.annotation.Nullable;
+
+import com.benmohammad.repeatrepeat.R;
+import com.benmohammad.repeatrepeat.data.Task;
+import com.benmohammad.repeatrepeat.tasks.domain.TaskFilters;
+import com.benmohammad.repeatrepeat.tasks.domain.TasksFilterType;
+import com.benmohammad.repeatrepeat.tasks.domain.TasksListModel;
+import com.google.common.collect.ImmutableList;
+
+import static com.google.common.collect.ImmutableList.*;
+
+import static com.benmohammad.repeatrepeat.tasks.view.EmptyTasksViewDataMapper.createEmptyTaskViewData;
+import static com.google.common.collect.Iterables.*;
+
+public class TasksListViewDataMapper {
+
+    public static TasksListViewData tasksListModelToViewData(TasksListModel model) {
+        return TasksListViewData.builder()
+                .loading(model.loading())
+                .filterLabel(getFilterLabel(model.filter()))
+                .viewState(getViewState(model.tasks(), model.filter()))
+                .build();
+    }
+
+    private static ViewState getViewState(@Nullable ImmutableList<Task> tasks, TasksFilterType filter) {
+        if(tasks == null) return ViewState.awaitingTasks();
+        ImmutableList<Task> filteredTasks = TaskFilters.filterTasks(tasks, filter);
+        if(filteredTasks.isEmpty()) {
+            return ViewState.emptyTasks(createEmptyTaskViewData(filter));
+        } else {
+            return ViewState.hasTasks(copyOf(transform(filteredTasks, TaskViewDataMapper::createTaskViewData)));
+        }
+    }
+
+    private static int getFilterLabel(TasksFilterType filterType) {
+        switch(filterType) {
+            case ACTIVE_TASKS:
+                return R.string.label_active;
+            case COMPLETED_TASKS:
+                return R.string.label_completed;
+            default:
+                return R.string.label_all;
+
+        }    }
+}
